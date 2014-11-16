e732612c9a84aa07ce30824cb3590d6095340185
Bunchhieng <Bunchhieng@gmail.com>
2014-11-15 23:48:53 -0500

    Initial commit

---
+++ assignments/helloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Your very first program in C - Hello world
+ *
+ * This program prints "Hello world!" to screen
+ *
+ */
+int main(int argc, char * argv[]){
+   
+   printf("Hello world!\n");
+
+   return 0;
+}
