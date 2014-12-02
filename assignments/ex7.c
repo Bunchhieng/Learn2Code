f9898afade8d99cad0aaf1fa3a351824c40a2baa
Bunchhieng <Bunchhieng@gmail.com>
2014-12-02 18:59:30 -0500

    Added AssnWeek2

---
+++ assignments/ex7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+   int n = 4, m = 5;
+
+   // if statement evaluate true or false
+   if (n + m < 8) {
+      printf("%d\n", n+m);
+   }
+   else {
+      printf("%d\n", n);
+   }
+
+   return 0;
+}
